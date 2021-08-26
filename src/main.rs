3cc05bd12279ea58140c9fee72dab7d1038246e9
Renning Bruns <nullren@github.com>
2021-08-26 16:27:58 -0700

    read prefixes from file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
-use std::io::{self, Write};
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+use std::ops::Deref;
+use std::path::Path;
 
-use ethers::prelude::{Signer};
-use ethers::signers::{coins_bip39::{Mnemonic, English}, MnemonicBuilder};
-use rand::{thread_rng};
+use ethers::prelude::Signer;
+use ethers::signers::{coins_bip39::{English, Mnemonic}, MnemonicBuilder};
+use rand::thread_rng;
 
 fn generate(mnemonic: &Mnemonic<English>) -> Result<(String, String), Box<dyn std::error::Error>> {
     let phrase: &str = &mnemonic.to_phrase()?;
-
     let wallet = MnemonicBuilder::<English>::default()
       .phrase(phrase)
       .build()?;
-
     Ok((format!("{:?}", wallet.address()), phrase.to_owned()))
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn load_prefixes() -> HashSet<String> {
+    let mut prefixes: HashSet<String> = HashSet::new();
+    if let Ok(lines) = read_lines("./ethaddrs.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                prefixes.insert(ip);
+            }
+        }
+    }
+    eprintln!("read {} prefixes", prefixes.len());
+    return prefixes;
 }
 
 fn main() {
     let mut rng = thread_rng();
     let mut count = 0u32;
+    let prefixes = load_prefixes();
     loop {
         count += 1;
         if count % 100 == 0 {
@@ -27,7 +52,15 @@ fn main() {
         let wallet = generate(&mnemonic);
         match wallet {
             Ok((address, phrase)) => {
-                if address.starts_with("0xdead") {
+                // ¯\_(ツ)_/¯
+                if prefixes.contains(&address[2..3]) ||
+                  prefixes.contains(&address[2..4]) ||
+                  prefixes.contains(&address[2..5]) ||
+                  prefixes.contains(&address[2..6]) ||
+                  prefixes.contains(&address[2..7]) ||
+                  prefixes.contains(&address[2..8]) ||
+                  prefixes.contains(&address[2..9]) ||
+                  prefixes.contains(&address[2..10]) {
                     eprintln!();
                     println!("{}: {}", address, phrase);
                 }
