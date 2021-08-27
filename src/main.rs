bf1e7e8283a5fb7706f23294c1a299ca29682867
Renning Bruns <nullren@github.com>
2021-08-27 11:28:27 -0700

    added rayon

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
-use std::ops::Deref;
 use std::path::Path;
 
 use ethers::prelude::Signer;
 use ethers::signers::{coins_bip39::{English, Mnemonic}, MnemonicBuilder};
 use rand::thread_rng;
+use ethers::types::Address;
 
-fn generate(mnemonic: &Mnemonic<English>) -> Result<(String, String), Box<dyn std::error::Error>> {
+fn to_wallet(mnemonic: &Mnemonic<English>) -> Result<(Address, String), Box<dyn std::error::Error>> {
     let phrase: &str = &mnemonic.to_phrase()?;
     let wallet = MnemonicBuilder::<English>::default()
       .phrase(phrase)
       .build()?;
-    Ok((format!("{:?}", wallet.address()), phrase.to_owned()))
+    Ok((wallet.address(), phrase.to_owned()))
 }
 
 // The output is wrapped in a Result to allow matching on errors
@@ -38,6 +38,24 @@ fn load_prefixes() -> HashSet<String> {
     return prefixes;
 }
 
+fn select_address(prefixes: &HashSet<String>, addr: Address) -> bool {
+    let address = format!("{:?}", addr);
+    // ¯\_(ツ)_/¯
+    (
+        prefixes.contains(&address[2..6]) &&
+          prefixes.contains(&address[38..42])
+    ) ||
+      (
+          &address[2..3] == &address[3..4] &&
+            &address[2..3] == &address[4..5] &&
+            &address[2..3] == &address[5..6] &&
+            &address[2..3] == &address[38..39] &&
+            &address[2..3] == &address[39..40] &&
+            &address[2..3] == &address[40..41] &&
+            &address[2..3] == &address[41..42]
+      )
+}
+
 fn main() {
     let mut rng = thread_rng();
     let mut count = 0u32;
@@ -49,20 +67,13 @@ fn main() {
             io::stderr().flush().unwrap();
         }
         let mnemonic = Mnemonic::new_with_count(&mut rng, 12).unwrap();
-        let wallet = generate(&mnemonic);
+        let wallet = to_wallet(&mnemonic);
         match wallet {
-            Ok((address, phrase)) => {
-                // ¯\_(ツ)_/¯
-                if prefixes.contains(&address[2..3]) ||
-                  prefixes.contains(&address[2..4]) ||
-                  prefixes.contains(&address[2..5]) ||
-                  prefixes.contains(&address[2..6]) ||
-                  prefixes.contains(&address[2..7]) ||
-                  prefixes.contains(&address[2..8]) ||
-                  prefixes.contains(&address[2..9]) ||
-                  prefixes.contains(&address[2..10]) {
+            Ok((addr, phrase)) => {
+                if select_address(&prefixes, addr) {
                     eprintln!();
-                    println!("{}: {}", address, phrase);
+                    println!("{:?}: {}: ({}): {}", addr, phrase, addr, count);
+                    count = 0;
                 }
             },
             Err(e) => {
